4c236cfd159348c1a68dfdb3856abcc6aad76468
martin byrne <marb61a@gmail.com>
2018-04-05 15:49:02 +0000

    added deploy js

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -10,6 +10,7 @@ const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 
 beforeEach(() => {
   // Get a list of all accounts
@@ -31,6 +32,18 @@ beforeEach(() => {
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-
+    assert.ok(inbox.options.address);
   });
+  
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+  
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
+  
 });
